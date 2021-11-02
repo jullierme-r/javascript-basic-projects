abe7c1cf649dd850dba61dbcac7bf517c50141db
Jullierme Rodrigues Gomes <jullierme.r.gomes@gmail.com>
2021-11-02 16:32:27 -0400

    clipboard
    possibility to copy the resulting css to the clipboard

--- 12-border-radius previewer/index.js
+++ 12-border-radius previewer/index.js
@@ -1,5 +1,7 @@
 const box = document.querySelector(".container");
 const radius = document.querySelectorAll(".radius");
+const textArea = document.querySelector("#clipboard");
+const btn = document.querySelector(".btn");
 
 let tl = 0;
 let tr = 0;
@@ -7,6 +9,7 @@ let bl = 0;
 let br = 0;
 
 radius.forEach(rad => {
+  //adds a change event to each corner input
   rad.addEventListener("change", ()=> {
     if (rad.id === "top-left") {
       tl = Number(rad.value);
@@ -18,5 +21,11 @@ radius.forEach(rad => {
       br = Number(rad.value);
     }
     box.style = `border-radius: ${tl}px ${tr}px ${br}px ${bl}px;`;
+    textArea.value = `border-radius: ${tl}px ${tr}px ${br}px ${bl}px;`;
   });
+});
+
+btn.addEventListener("click", () => {
+  textArea.select(); //select all the text in the area
+  navigator.clipboard.writeText(textArea.value); //writes the text to the clipboard
 });
